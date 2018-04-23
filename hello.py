60d6cf0731ac45e973675849aa2fa882dcab513f
ifatregev <ifatregev007@gmail.com>
2018-04-23 19:09:48 +0300

    Add CORS support

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
-    return '<html><a href="/bubi" style="color: red">Go to Bubi</a></html>'
+    return "Ifat's RESTful API"
 
-@app.route("/bubi")
-def bubi():
-    return 'Bubi is the best'
+@app.route("/product")
+def product():
+    productList = [
+        {
+            'name': 'Bubi Antibody',
+            'rating': 5
+        }
+    ]
+    return jsonify(productList)
 
 if __name__ == "__main__":
     app.run()
