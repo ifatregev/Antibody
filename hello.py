e967e900af5cc8e526d4b28a08e82f375d164f4f
ifatregev <ifatregev007@gmail.com>
2018-05-09 17:41:26 +0300

    Create product end point

--- hello.py
+++ hello.py
@@ -8,15 +8,28 @@ CORS(app)
 def hello():
     return "Ifat's RESTful API"
 
-@app.route("/product")
-def product():
-    productList = [
-        {
-            'name': 'Bubi Antibody',
-            'rating': 5
-        }
-    ]
-    return jsonify(productList)
+productList = [
+    {
+        'name': 'Bubi Antibody',
+        'rating': 5
+    },
+    {
+        'name': 'Bubi2 Antibody',
+        'rating': 4
+    },
+    {
+        'name': 'Bubi3 Antibody',
+        'rating': 3
+    }
+]
+
+@app.route("/product/")
+@app.route("/product/<int:product_id>")
+def product(product_id=None):
+    if product_id:
+        return jsonify(productList[product_id])
+    else:
+        return jsonify(productList)
 
 if __name__ == "__main__":
     app.run()
